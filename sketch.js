d4cf65daad26e2192ab4b9db917ec14d9f4a0dfc
websites-dotcom <0205.akshatgupta@gmail.com>
2021-03-18 17:49:37 +0530

    added slideshow

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var n = 0;
+var images = [];
+var time = 2000;
+
+images[0] = 'img/arrow-2.jpg';
+images[1] = 'img/arrow-3.jpg';
+images[2] = 'img/arrow-4.jpg';
+images[3] = 'img/arrow-5.jpg';
+
+var changeImg = function(){
+
+	document.slide.src = images[n];
+
+	if(n<images.length - 1){
+		n++;
+	}
+	else{
+		n = 0;
+	}
+
+	setTimeout("changeImg()", time);
+
+}
+
+window.onload = changeImg;
